1528cab9d941ab4ca8d7f9c0942cb7c088df2065
Martincho83 <martinlamas83@gmail.com>
2024-06-08 22:34:36 -0300

    Esquemas carrera, materia. Middleware para validacion. Integrar la validacion en las rutas

--- models/tipo_grado.js
+++ models/tipo_grado.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Tipo_Grado.hasMany(models.Carrera, { foreignKey: 'grado', as: 'carreras' });
     }
   }
   Tipo_Grado.init({
