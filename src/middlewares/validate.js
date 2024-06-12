5e53659ffc30ebc351dc22aa16fc67dd0ae94b23
uciel <ucibustamante.a@gmail.com>
2024-06-11 21:05:56 -0300

    middleware

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,13 +1,36 @@
-const Joi = require('joi');
+const models = {
+  Universidad: require('./models/Universidad'),
+  Carrera: require('./models/Carrera'),
+  TipoGrado: require('./models/TipoGrado'),
+  Materia: require('./models/Materia'),
+  CarreraMaterias: require('./models/CarreraMaterias')
+};
 
-const validate = (schema) => {
-  return (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
-      return res.status(400).json({ message: error.details[0].message });
+const validateEntityExists = (entity) => {
+  return async (req, res, next) => {
+    const { id } = req.params;
+    const model = models[entity];
+    const record = await model.findByPk(id);
+    if (!record) {
+      return res.status(404).json({ message: `${entity} no encontrado/a` });
     }
     next();
   };
 };
 
-module.exports = validate;
+
+const validateCarreraMateriasExists = async (req, res, next) => {
+  const { id_materia, id_carrera } = req.params;
+  const record = await models.CarreraMaterias.findOne({
+    where: { id_materia, id_carrera }
+  });
+  if (!record) {
+    return res.status(404).json({ message: 'Asociación de carrera y materias no encontrada' });
+  }
+  next();
+};
+
+module.exports = {
+  validateEntityExists,
+  validateCarreraMateriasExists
+};
